c2b3a6a16e3266b2e274c0cb76deb76ff82836dd
soulcounter486-lgtm <256628752+soulcounter486-lgtm@users.noreply.github.com>
2026-02-21 01:24:03 -0500

    Force resolve case-sensitive issue by renaming toaster to toaster-new

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster-new";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { LanguageProvider } from "@/lib/i18n";
 import { useAuth } from "@/hooks/use-auth";
